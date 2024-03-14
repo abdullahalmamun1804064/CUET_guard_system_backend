b53bad45b66225ff11543161fb948ba074135dbf
Abdullah Al Mamun <abdullahalmamun1804064@gmail.com>
2024-03-14 14:46:14 +0600

    latest code

--- routes/post.js
+++ routes/post.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const { getSinglePost, getAllPost, create, update, deletePost } = require('../controllers/postController');
+const { isAuthenticatedUser } = require('../middlewares/auth');
 
 router.get('/', getAllPost);
 router.post('/', create);
 router.get('/:id', getSinglePost)
-router.put('/:id', update)
+router.put('/:id',isAuthenticatedUser, update)
 router.delete('/:id', deletePost);
 
 
